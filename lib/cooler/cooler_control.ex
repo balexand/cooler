0209bcfd9ab85114c4e838e9996d462952ab85ff
balexand <balexand@gmail.com>
2016-08-30 12:59:02 -0600

    give the pad 30 seconds to get wet

--- lib/cooler/cooler_control.ex
+++ lib/cooler/cooler_control.ex
@@ -15,18 +15,33 @@ defmodule Cooler.CoolerControl do
   def toggle do
     Agent.update :cooler_control, fn state ->
       case state do
-        :on ->
-          Gpio.write(:pump_relay, 1)
-          Gpio.write(:motor_relay, 1)
-
-          :off
-        :off ->
-          Gpio.write(:pump_relay, 0)
-          :timer.sleep(1000)
-          Gpio.write(:motor_relay, 0)
-
-          :on
+        :on -> do_turn_off()
+        :off -> do_turn_on()
       end
     end
+  end
+
+  defp do_turn_on do
+    Gpio.write(:pump_relay, 0)
+
+    parent = self()
+
+    spawn_link fn ->
+      # give the pad time to get wet then start the motor
+      :timer.sleep(30_000)
+
+      Agent.get parent, fn state ->
+        if state == :on, do: Gpio.write(:motor_relay, 0)
+      end
+    end
+
+    :on
+  end
+
+  defp do_turn_off do
+    Gpio.write(:pump_relay, 1)
+    Gpio.write(:motor_relay, 1)
+
+    :off
   end
 end
